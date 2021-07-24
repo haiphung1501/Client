b886a3ed365d778e66fdef8ec4cd1840c345cfee
haiphung1501 <haiphung15012002@gmail.com>
2021-07-24 21:15:43 +0700

    Changes

--- Demo_Client/Demo_Client.cpp
+++ Demo_Client/Demo_Client.cpp
@@ -3,6 +3,8 @@
 #include "afxsock.h"
 #include <string>
 #include <fstream>
+#include <windows.h>
+#include <conio.h>
 
 #ifdef _DEBUG
 #define new DEBUG_NEW
@@ -12,21 +14,81 @@
 // The one and only application object
 
 CWinApp theApp;
-
 using namespace std;
-DWORD WINAPI function_cal(LPVOID arg) {
-	SOCKET* hConnected = (SOCKET*)arg;
-	CSocket mysock;
-	//Chuyen ve lai CSocket
-	mysock.Attach(*hConnected);
 
+string passwordInput(unsigned maxLength)
+{
+	string pw;
+	for (char c; (c = _getch()); )
+	{
+		if (c == '\n' || c == '\r') { //Enter
+			cout << "\n";
+			break;
+		}
+		else if (c == '\b') { // Backspace
+			cout << "\b \b";
+			if (!pw.empty()) pw.erase(pw.size() - 1);
+		}
+		else if (c == -32) { //up down right left
+			_getch();
+		}
+		else if (isprint(c) && pw.size() < maxLength) {
+			std::cout << '*';
+			pw += c;
+		}
+	}
+	return pw;
+}
+void login(string& user, string& pass) {
+	cout << "Enter Username:";
+	getline(cin, user, '\n');
+
+	cout << "Enter password:";
+	pass = passwordInput(30);
+}
+
+DWORD WINAPI function_cal1(LPVOID arg) {
+	SOCKET* hConnected = (SOCKET*)arg;
+	CSocket client;
+	//Chuyen ve lai CSocket
+	client.Attach(*hConnected);
+	int number_continue = 0;
+	char letter;
+	do {
+		fflush(stdin);
+		int number_a, number_b, number_result;
+		char letter;
+		printf("Nhap vao phep toan (+, -): ");
+		scanf("%c", &letter);
+
+		printf("Nhap vao so thu nhat: ");
+		scanf("%d", &number_a);
+
+		printf("Nhap vao so thu hai: ");
+		scanf("%d", &number_b);
+
+		//Gui phep toan den server
+		client.Send(&letter, sizeof(letter), 0);
+		//Gui so thu nhat den server
+		client.Send(&number_a, sizeof(number_a), 0);
+		//Gui so thu hai den server
+		client.Send(&number_b, sizeof(number_b), 0);
+
+		//Nhan ket qua tinh toan tu server
+		client.Receive(&number_result, sizeof(number_result), 0);
+		printf("Ket qua phep toan %d %c %d=%d\n", number_a, letter, number_b, number_result);
+
+		printf("Nhap 1 de tiep tuc, 0 de thoat: ");
+		scanf("%d", &number_continue);
+		client.Send(&number_continue, sizeof(number_continue), 0);
+	} while (number_continue);
 
 	//Code
-	cout << "Hello";
+	cout << "Goi duoc Function Client";
 	//Code
 
 	delete hConnected;
-	mysock.Close();
+	client.Close();
 	return 0;
 }
 int _tmain(int argc, TCHAR* argv[], TCHAR* envp[])
@@ -60,15 +122,22 @@ int _tmain(int argc, TCHAR* argv[], TCHAR* envp[])
 			//cin >> port;
 			//cin.ignore();
 			client.Create();
-			if (client.Connect(CA2W(ip), port))
-			{
+			int number_continue = 0;
+			char letter;
+			if (client.Connect(CA2W(ip), port)) {
 				cout << "Succesfully Connected!" << endl;
 
 				SOCKET* hConnected = new SOCKET();
 				*hConnected = client.Detach();
-				threadStatus = CreateThread(NULL, 0, function_cal, hConnected, 0, &threadID);
+				threadStatus = CreateThread(NULL, 0, function_cal1, hConnected, 0, &threadID);
 
 				client.Attach(*hConnected);
+
+				while (1) {
+					DWORD dwWait = WaitForSingleObject(threadStatus, 0); // timeout right away if thread handle is not signalled
+					if (dwWait == WAIT_OBJECT_0)
+						exit(0);
+				}
 			}
 			else
 				cout << "Ket noi khong thanh cong" << endl;
